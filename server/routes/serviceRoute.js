75110a651697d0dcc340284e08265f71282b3c91
minhuyen <minhuyen0210@gmail.com>
2022-04-04 14:05:51 +0700

    change api

--- server/routes/serviceRoute.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const { CreateFile } = require('../controllers/create-file')
-
-router.get('/report', CreateFile);
-
-module.exports = router;
